5c2ce4ce3df8b0d393aa132625dba0bf8cb1a12f
RISHII7 <rishikesh.palande07@gmail.com>
2024-11-02 22:53:02 +0530

    #3 Customizing components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
-import { Component } from "@/features/component";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <div>
-      <Component>
-
-      </Component>
+    <div className="flex flex-col items-center justify-center  gap-[100px]">
+      <Button variant="primary"> Primary </Button>
+      <Button variant="secondary"> Secondary </Button>
+      <Button variant="destructive"> Destructive </Button>
+      <Button variant="ghost"> Ghost </Button>
+      <Button variant="muted"> Muted </Button>
+      <Button variant="outline"> Outline </Button>
+      <Button variant="teritary"> Teritrary </Button>
     </div>
   );
 }
