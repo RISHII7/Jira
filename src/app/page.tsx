6cbdfdf0e51c91777b2a07af4c61aa4870e9bb4e
RISHII7 <rishikesh.palande07@gmail.com>
2024-11-02 19:00:23 +0530

    #2 Adding a component library

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
-import Image from "next/image";
+import { Component } from "@/features/component";
 
 export default function Home() {
   return (
     <div>
-      Jira Cloning in Progress!!
+      <Component>
+
+      </Component>
     </div>
   );
 }
---
+++ src/features/component.tsx
@@ -0,0 +1,7 @@
+export const Component = () => {
+    return (
+        <div className="text-center">
+            Hello Test Component
+        </div>
+    )
+}
