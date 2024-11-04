84d44a509b336954662e8eaa5acd3a30aa43065f
RISHII7 <rishikesh.palande07@gmail.com>
2024-11-04 19:39:39 +0530

    #6 Setting up Hono API

---
+++ src/app/api/[[...route]]/route.ts
@@ -0,0 +1,11 @@
+import { Hono } from "hono";
+import { handle } from "hono/vercel";
+
+const app = new Hono().basePath("/api");
+
+app.get("/project/:projectId", (c) => {
+    const projectId = c.req.param("projectId")
+    return c.json({ project: projectId })
+});
+
+export const GET = handle(app);
